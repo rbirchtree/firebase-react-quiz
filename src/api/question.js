0a8515f75e0428208973df5fb1438a5b0b993f29
Rob Birch <rbirchtree@gmail.com>
2019-04-26 08:02:26 -0700

    working prototype

---
+++ src/api/question.js
@@ -0,0 +1,29 @@
+var quizQuestions = [
+    {
+        question: "What MLB team is the best in the NL West?",
+        answers: [
+            {
+                type: "San Francisco",
+                content: "Giants"
+            },
+            {
+                type: "Colorado",
+                content: "Rockies"
+            },
+            {
+                type: "Arizona",
+                content: "Diamondbacks"
+            },
+            {
+                type: "Los Angeles",
+                content: "Dodgers"
+            },
+            {
+                type: "San Diego",
+                content: "Padres"
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
